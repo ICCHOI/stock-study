2ae641bc59694a00fd91c9ee07d7292e1f3c0b27
ICCHOI <cicblck@gmail.com>
2022-12-24 20:38:40 +0900

    redis lettuce 실습

---
+++ src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,31 @@
+package com.example.stock.facade;
+
+import org.springframework.stereotype.Component;
+
+import com.example.stock.repository.RedisRepository;
+import com.example.stock.service.StockService;
+
+@Component
+public class LettuceLockStockFacade {
+
+	private RedisRepository redisRepository;
+
+	private StockService stockService;
+
+	public LettuceLockStockFacade(RedisRepository redisRepository, StockService stockService) {
+		this.redisRepository = redisRepository;
+		this.stockService = stockService;
+	}
+
+	public void decrease(Long key, Long quantity) throws InterruptedException {
+		while (!redisRepository.lock(key)) {
+			Thread.sleep(100);
+		}
+
+		try {
+			stockService.decrease(key, quantity);
+		} finally {
+			redisRepository.unlock(key);
+		}
+	}
+}
